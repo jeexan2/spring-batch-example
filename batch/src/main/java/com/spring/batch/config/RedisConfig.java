921b0375b4284305cdfe05dce6dcff3d3d26686c
Jysan Aziz <jeexan2@gmail.com>
2022-03-18 02:18:32 +0600

    redis and  batch dependency added

---
+++ batch/src/main/java/com/spring/batch/config/RedisConfig.java
@@ -0,0 +1,38 @@
+package com.spring.batch.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Configuration
+public class RedisConfig {
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.setHostName(redisHost);
+        jedisConnectionFactory.setPort(redisPort);
+        return jedisConnectionFactory;
+    }
+
+    @Bean
+    RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+
+        redisTemplate.setDefaultSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
---
+++ batch/src/main/java/com/spring/batch/dao/RedisDao.java
@@ -0,0 +1,48 @@
+package com.spring.batch.dao;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Data
+@Slf4j
+@NoArgsConstructor
+@Service
+public class RedisDao {
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    public void setValueWithTimeout(String key, String value, int timeOut) {
+        ValueOperations<String, String> ops = this.redisTemplate.opsForValue();
+        ops.set(key, value, timeOut, TimeUnit.SECONDS);
+    }
+    public void setValue(String key, String value) {
+        ValueOperations<String, String> ops = this.redisTemplate.opsForValue();
+        ops.set(key, value);
+    }
+
+    public boolean hasKey(String key) {
+        return this.redisTemplate.hasKey(key);
+    }
+
+    public String getValue(String key) {
+        ValueOperations<String, String> ops = this.redisTemplate.opsForValue();
+        return ops.get(key);
+    }
+
+    public void deleteValue(String key) {
+        ValueOperations<String, String> ops = this.redisTemplate.opsForValue();
+        ops.getOperations().delete(key);
+    }
+
+    public long increment(String key) {
+        ValueOperations<String, String> ops = this.redisTemplate.opsForValue();
+        return ops.increment(key, 1);
+    }
+}
